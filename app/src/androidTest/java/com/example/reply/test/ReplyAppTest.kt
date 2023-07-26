7abe2c0661bcf2a3933cdd351f93f302593761f4
CheoR <cheo.roman+github@gmail.com>
2023-07-26 14:41:59 -0500

    test:  use annotations to group tests for different screen sizes

--- app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -13,6 +13,7 @@ class ReplyAppTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
+    @TestCompactWidth
     fun compactDevice_verifyUsingBottomNavigation() {
         // Set up compact window
         composeTestRule.setContent {
@@ -29,6 +30,7 @@ class ReplyAppTest {
     }
 
     @Test
+    @TestMediumWidth
     fun mediumDevice_verifyUsingNavigationRail() {
         // Set up medium window
         composeTestRule.setContent {
